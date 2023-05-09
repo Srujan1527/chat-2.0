02a774439e2386f2493c29ba931facc3c7205fe9
Simhadri131 <Simhadri@coretek.io>
2023-05-09 16:28:42 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function Home() {
+  const [pass, setPass] = useState(false);
+  const [color, setColor] = useState(false);
+  const router = useRouter();
+  const passwordToEnter = "PottiNanni";
+
+  const { register, handleSubmit } = useForm();
+
+  const getData = (data: any) => {
+    const EnterPassword = data.ManualPassword;
+    console.log(EnterPassword);
+    const passwordLength = EnterPassword.length > 5;
+    setColor(passwordLength);
+    setInterval(() => {
+      if (EnterPassword === passwordToEnter) {
+        router.push("/message");
+      }
+    }, 5000);
+  };
+  return (
+    <div className="grid h-screen w-full grid-cols-1 place-content-center place-items-center bg-[url('https://media.istockphoto.com/id/688387642/photo/rose-background.jpg?b=1&s=170667a&w=0&k=20&c=JUutccKTZcDDzTVUSGUAzDnrJA5nyXs7FR9VJsZVnB4=')]">
+      <div className="bg-[url('https://media.istockphoto.com/id/688387642/photo/rose-background.jpg?b=1&s=170667a&w=0&k=20&c=JUutccKTZcDDzTVUSGUAzDnrJA5nyXs7FR9VJsZVnB4=')] grid grid-cols-1 ">
+        <form onSubmit={handleSubmit(getData)}>
+          <div className="flex flex-col justify-center">
+            <div className="mb-10">
+              <input
+                type="text"
+                className="w-full rounded-md text-white text-center font-bold  bg-transparent border-4 border-white py-2 px-10 "
+                placeholder="I Love You Nanni"
+                {...register("ManualPassword")}
+              />
+            </div>
+            {color ? (
+              <div className="rounded-lg flex justify-center py-2 mt-4 bg-transparent border-4 border-white text-white">
+                <button type="submit">please Wait Nanni</button>
+              </div>
+            ) : (
+              <div className="rounded-md flex justify-center py-2 mt-4 bg-blue-950 text-white">
+                <button>Enter</button>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ pages/message.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import localStorage from "localStorage";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+function Message() {
+  const [message, setMessage] = useState("");
+  const [arrayData, setArrayData] = useState([] as any);
+  const router = useRouter();
+  // const [resultArray,setResultArray]=useState([])
+  useEffect(() => {
+    const getResultFromStorage = () => {
+      const storedMessages = JSON.parse(localStorage.getItem("message")) || [];
+      setArrayData(storedMessages);
+    };
+    getResultFromStorage();
+  }, []);
+
+  const sendMessage = (e: any) => {
+    e.preventDefault();
+    setArrayData([...arrayData, message]);
+    const localData = localStorage.setItem(
+      "message",
+      JSON.stringify([...arrayData, message])
+    );
+    setMessage("");
+  };
+
+  const handleDelete = () => {
+    localStorage.clear();
+    router.reload();
+  };
+
+  https: return (
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% overflow-auto h-screen w-full">
+      <div className="flex justify-center items-center">
+        <h1 className="text-white hover:text-red-700 hover:my-8 text-center font-serif font-extrabold relative">
+          I ALWAYS LOVE YOU POTTI
+        </h1>
+        <button
+          className=" bg-transparent border-2 ml-1 border-red-950 text-white px-4 py-1 rounded-md "
+          onClick={handleDelete}
+        >
+          Clear Chat
+        </button>
+      </div>
+      <ul className="w-full">
+        {arrayData.map((each, index) => (
+          <li key={index}>
+            <div className="text-white   break-words font-bold hover:text-red-950 hover:px-10 bg-transparent border-2 hover:border-black rounded-lg  mx-4  hover:border-dotted  border-white px-2 my-1">
+              {each}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <form className="absolute bottom-2 w-full" onSubmit={sendMessage}>
+        <input
+          className="py-2 bg-pink-800  hover:bg-blue-950   border-black border-2 text-white rounded-lg px-4 w-[75%] ml-5"
+          placeholder="Enter Your Text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className=" font-bold hover:bg-green-950  bg-pink-800 rounded-lg text-black px-2 py-2"
+          type="submit"
+        >
+          send
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Message;
