cce1e9d491d5c7c33b73e56ecb85cbe03a64b032
SrujanCoretek <SrujanP@coretek.io>
2023-05-15 17:31:41 +0530

    responsiveness

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -65,123 +65,124 @@ const SignUp = () => {
     }
   };
   return (
-    <div data-testid="sign-up" className="min-h-screen ">
-      <form className="border-2 w-full  h-screen flex justify-center flex-col items-center">
-        <div className="w-1/2 border-2 p-4 rounded-lg ">
+    <div data-testid="sign-up" className="min-h-screen">
+        <form className="border-2 w-full h-screen flex justify-center flex-col items-center">
+            <div className="w-11/12 md:w-1/2 border-2 p-4 rounded-lg">
 
-        <div className="heading-container">
-          <h2 className="mt-4 mb-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Do not have an account</h2>
-          <span className="mt-4 ml-8  mb-14 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up with your email</span>
-        </div>
-        <div className="mb-4 ml-8">
-          <label htmlFor="name"className="block text-sm font-medium leading-6 text-gray-900">
-            Name
-          </label>
-          <input
-            className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-           
-            type="text"
-            id="name"
-            required
-            onChange={handleChange}
-            name="displayName"
-            placeholder="Enter your Name"
-            value={displayName}
-          />
-        </div>
-        <div className="mb-4 ml-8">
-          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-            Email
-          </label>
-          <input
+            <div className="heading-container">
+              <h2 className="mt-4 mb-6 md:mb-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Do not have an account</h2>
+              <span className="mt-4 ml-2 md:ml-8 mb-10 md:mb-14 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up with your email</span>
+            </div>
             
-            className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            type="email"
-            required
-            id="email"
-            onChange={handleChange}
-            name="email"
-            placeholder="Enter your Email"
-            value={email}
-          />
-        </div>
-
-        <div className="mb-4 ml-8">
-          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-            Password
-          </label>
-          <input
-            className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <input
+                className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                type="text"
+                id="name"
+                required
+                onChange={handleChange}
+                name="displayName"
+                placeholder="Enter your Name"
+                value={displayName}
+              />
+            </div>
             
-            type="password"
-            required
-            id="password"
-            onChange={handleChange}
-            name="password"
-            placeholder="Enter your password"
-            value={password}
-          />
-        </div>
-        <div className="mb-4 ml-8">
-          <label htmlFor="c-password" className="block text-sm font-medium leading-6 text-gray-900">
-            Confirm Password
-          </label>
-          <input
-          
-          className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            type="password"
-            required
-            id="c-password"
-            onChange={handleChange}
-            name="confirmPassword"
-            placeholder="Enter your confirm password"
-            value={confirmPassword}
-          />
-        </div>
-        <div className="mb-4 ml-8">
-          <label htmlFor="phoneNumber"className="block text-sm font-medium leading-6 text-gray-900">
-            PhoneNumber
-          </label>
-          <input
-        
-        className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            type="number"
-            required
-            id="phoneNumber"
-            onChange={handleChange}
-            name="phoneNumber"
-            placeholder="Enter your phoneNumber"
-            value={phoneNumber}
-          />
-        </div>
-        <div className="mb-4 ml-8">
-          <label htmlFor="profession" className="block text-sm font-medium leading-6 text-gray-900">
-            Profession
-          </label>
-          <input
-      
-      className="block  p-2 w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            type="text"
-            required
-            id="profession"
-            onChange={handleChange}
-            name="profession"
-            placeholder="Enter your profession"
-            value={profession}
-          />
-        </div>
-        {user && (
-          <span className="message">User has been successfully Created</span>
-        )}
-        <div className="flex ml-8  mt-4 justify-start items-center">
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email
+              </label>
+              <input
+                className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                type="email"
+                required
+                id="email"
+                onChange={handleChange}
+                name="email"
+                placeholder="Enter your Email"
+                value={email}
+              />
+            </div>
 
-        <button type="submit" onClick={signUp} className=" w-20 mb-2 mr-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          SignUp
-        </button>
-        </div>
-        </div>
-      </form>
-    </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <input
+                className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                type="password"
+                required
+                id="password"
+                onChange={handleChange}
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+              />
+            </div>
+            <div className="mb-4">
+        <label htmlFor="c-password" className="block text-sm font-medium leading-6 text-gray-900">
+          Confirm Password
+        </label>
+        <input
+          className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          type="password"
+          required
+          id="c-password"
+          onChange={handleChange}
+          name="confirmPassword"
+          placeholder="Enter your confirm password"
+          value={confirmPassword}
+          />
+          </div>
+          <div className="mb-4">
+    <label htmlFor="phoneNumber" className="block text-sm font-medium leading-6 text-gray-900">
+      PhoneNumber
+    </label>
+    <input
+      className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      type="number"
+      required
+      id="phoneNumber"
+      onChange={handleChange}
+      name="phoneNumber"
+      placeholder="Enter your phoneNumber"
+      value={phoneNumber}
+    />
+  </div>
+
+  <div className="mb-4">
+    <label htmlFor="profession" className="block text-sm font-medium leading-6 text-gray-900">
+      Profession
+    </label>
+    <input
+      className="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      type="text"
+      required
+      id="profession"
+      onChange={handleChange}
+      name="profession"
+      placeholder="Enter your profession"
+      value={profession}
+    />
+  </div>
+
+  {user && (
+    <span className="message">User has been successfully Created</span>
+  )}
+
+  <div className="flex mt-6 justify-center items-center">
+    <button type="submit" onClick={signUp} className="w-32 mb-2 md:mr-4 flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      SignUp
+    </button>
+  </div>
+  
+</div>
+</form>
+</div>
+
+  
   );
 };
 
